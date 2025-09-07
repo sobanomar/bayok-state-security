4943ca09b966b60c2315592c48d1e0dfc6734789
sobanomar <sobanumar0@gmail.com>
2025-09-07 12:28:21 +0500

    FIX update business card text

--- src/pages/AboutUs/AboutUsPage.jsx
+++ src/pages/AboutUs/AboutUsPage.jsx
@@ -80,7 +80,7 @@ const AboutUsPage = () => {
               px-4 md:px-8 transition-all duration-500 cursor-pointer py-1.5 
              whitespace-normal break-words text-center"
                           >
-                            View Business Card
+                            Business Card
                           </a>
                         </div>
                       </div>
